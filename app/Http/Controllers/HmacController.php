3bd5e2585b974f6562ca01fe3d371ea4237afcd2
Medshot <chornogovlada@gmail.com>
2019-03-19 11:38:18 +0200

    add hmac check controller

---
+++ app/Http/Controllers/HmacController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class HmacController extends Controller
+{
+    public static function hmac_calc($query, $secret_key){
+        $one_minute_ago = Carbon::now()->subSeconds(60)->timestamp;
+        if ($query['timestamp'] < $one_minute_ago) {
+            return false;
+            exit;
+        }
+        $hmac = $query['hmac'];
+        unset($query['hmac']);
+        foreach ($query as $key => $val) {
+            $params[] = "$key=$val";
+        }
+        asort($params);
+        $params = implode('&', $params);
+        $calculated_hmac = hash_hmac('sha256', $params, $secret_key);
+        if ($hmac == $calculated_hmac){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
